7471bfda9ba52c72a81fe6bf2a88d4238047fb11
Matthew Li <m739li@uwaterloo.ca>
2021-11-30 15:36:57 -0500

    first commit

---
+++ src/Image.js
@@ -0,0 +1,21 @@
+
+
+function Image() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://www.gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello
+          </a>
+          <img alt = "birds" src ="https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk="/>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Image;
